0b2a95d8b7ba3890a5188aac2a7e9641834bd49e
unknown <565271104@qq.com>
2020-01-02 22:44:13 +0800

    firstVue commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+<script>
+import VHeader from './components/v-header/v-header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+#app {
+  .tab-wrapper {
+    position: fixed;
+    top: 136px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+}
+</style>
